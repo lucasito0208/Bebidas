99cbfd3c749bf6273426045d34871783c4047101
Lucas <93831338+lucasito0208@users.noreply.github.com>
2022-05-03 14:38:33 +0200

    final

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = () => {
+    return(
+        <header className="bg-alert">
+            <h1>Muestra recetas de tragos</h1>
+        </header>
+    );
+}
+
+export default Header;
+
+
